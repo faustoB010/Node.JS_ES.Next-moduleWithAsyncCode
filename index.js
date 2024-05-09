030355597ad42a69adb8d67a60d8f1ae9ab1ce66
Fausto <mcoxxrd@hotmail.com>
2024-05-08 21:14:52 -0500

    First Commit

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
-const express = require('express');
+import { routeHello, routeAPINames } from "./routes.js";
+import express from "express";
+
+
 const server = express();
 const port = 1000;
 server.get('/hello', function (req, res) {
-res.send('Hello World!');
+const response = routeHello(req, res);
+res.send(response);
 });
-server.listen(port, function () {
-console.log('Listening on ' + port);
+server.get("/api/names", async function (req, res) {
+    let response;
+    try {
+    response = await routeAPINames(req, res);
+    } catch (err) {
+    console.log(err);
+    }
+    res.send(response);
+    
+    });
+    server.listen(port, function () {
+    console.log("Listening on " + port);
 });
